3474c565c0290914237926e280c805faa2b55781
KatsushiHegi <67356385+KatsushiHegi@users.noreply.github.com>
2021-01-27 00:29:14 +0900

    hp

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -34,7 +34,7 @@ public class Player : People
         {
             return new Player()
             {
-                Hp = 100,
+                Hp = 1000,
                 MaxHp = 1000,
                 Atk = 100,
             };
